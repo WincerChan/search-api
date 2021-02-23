94460eba0e419df8fcec1ee4c5a08327659722fd
WincerChan <WincerChan@gmail.com>
2021-02-24 00:15:08 +0800

    unix domain socket

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use actix_web::{get, web, HttpResponse, Result};
 use chrono::NaiveDate;
-use serde::{Deserialize, Serialize,
-    de::{Deserializer, Error, Unexpected}
+use serde::{
+    de::{Deserializer, Error, Unexpected},
+    Deserialize, Serialize,
 };
-use std::{env, fs};
-use tantivy::{
-    collector::Count,
-    schema::Value,
-    query::Query
-};
+use std::{env, io::BufWriter};
+use tantivy::{collector::Count, query::Query, schema::Value};
 
 mod config;
 mod search;
@@ -101,10 +98,7 @@ where
 }
 
 #[get("/")]
-async fn greet(
-    info: web::Query<Thing>,
-    qs: web::Data<QuerySchema>,
-) -> Result<HttpResponse> {
+async fn greet(info: web::Query<Thing>, qs: web::Data<QuerySchema>) -> Result<HttpResponse> {
     let query_schema = qs.get_ref();
     let mut box_qs: Vec<Box<dyn Query>> = Vec::new();
     let (keyword_query, true_query) = query_schema.make_keyword_query(&info.q);
@@ -157,32 +151,79 @@ async fn greet(
         data: results,
     }))
 }
+// async fn serv_query(mut stream: UnixStream) {
+//     println!("first");
+//     stream.readable().await.unwrap();
+//     println!("fjdljafl");
+//     let mut response = Vec::new();
+//     stream.try_read(&mut response);
+//     println!("{:#?}", response);
+// }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let v = env::args().collect::<Vec<String>>();
-    let config = config::read_config();
-    match v.last() {
-        Some(cli) => {
-            if cli == "init" {
-                #[warn(unused_must_use)]
-                fs::remove_dir_all(&config.tantivy_db).unwrap();
-                migrate::init_schema(&config.tantivy_db, &config.blog_source);
-                println!("Initial Tantivy Schema Succeed!");
-                return Ok(());
-            } else if cli == "migrate" {
-                migrate::init_schema(&config.tantivy_db, &config.blog_source);
-                println!("Initial Tantivy Schema Succeed!");
-                return Ok(());
-            }
-        },
-        None => ()
+// async fn loop_accept(path: &str) {
+//     let listener = UnixListener::bind(path).await.unwrap();
+//     let mut incoming = listener.incoming();
+//     while let Some(stream) = incoming.next().await {
+//         let stream = stream.unwrap();
+//         find_result(stream)
+//     }
+// }
+use std::io::prelude::*;
+use std::io::{BufRead, BufReader, LineWriter};
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::thread;
+
+fn handle_client(stream: UnixStream) {
+    let stream_reader = BufReader::new(&stream);
+    let mut stream_writer = LineWriter::new(&stream);
+    for line in stream_reader.lines() {
+        stream_writer.write("fjlfjl;a\n".as_bytes());
+        println!("{}", line.unwrap());
     }
-    use actix_web::{App, HttpServer};
-    let qs = QuerySchema::new(&config.tantivy_db);
-
-    HttpServer::new(move || App::new().data(qs.clone()).service(greet))
-        .bind(config.listen_addr)?
-        .run()
-        .await
+    println!("end")
 }
+
+fn main() {
+    let listener = UnixListener::bind("/tmp/rust-uds.sock").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| handle_client(stream));
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
+}
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     let v = env::args().collect::<Vec<String>>();
+//     let config = config::read_config();
+//     loop_accept(&config.listen_addr);
+//     match v.last() {
+//         Some(cli) => {
+//             if cli == "init" {
+//                 migrate::create_dir(&config.tantivy_db);
+//                 migrate::init_schema(&config.tantivy_db, &config.blog_source);
+//                 println!("Initial Tantivy Schema Succeed!");
+//                 return Ok(());
+//             } else if cli == "migrate" {
+//                 migrate::init_schema(&config.tantivy_db, &config.blog_source);
+//                 println!("Initial Tantivy Schema Succeed!");
+//                 return Ok(());
+//             }
+//         }
+//         None => (),
+//     }
+//     use actix_web::{App, HttpServer};
+//     let qs = QuerySchema::new(&config.tantivy_db);
+//     println!("Listening: {:#?}", config.listen_addr);
+
+//     HttpServer::new(move || App::new().data(qs.clone()).service(greet))
+//         .bind(config.listen_addr)?
+//         .run()
+//         .await
+// }

309c2797d70e299755033f63659e6dcdc53dd7ee
WincerChan <WincerChan@gmail.com>
2021-04-18 15:27:48 +0800

    update decode function

--- src/ipc/decode.rs
+++ src/ipc/decode.rs
@@ -1,65 +1,63 @@
 use std::{convert::TryInto, io::Read, os::unix::net::UnixStream};
 
-static LENGTH_SIZE: usize = 8;
+static LENGTH_SIZE: usize = 4;
+static INTEGER_SIZE: usize = 8;
+static DATA_OFFSET: usize = 3; // 3 = 1 (delimeter) + 1 (basic or compound) + 1 (contains element type)
 
 fn extract_length(stream: &mut UnixStream) -> usize {
     let mut len_buf = vec![0; LENGTH_SIZE];
     stream.read(&mut len_buf).expect("err get length");
-    usize::from_be_bytes(len_buf.try_into().expect("error to int"))
+    u32::from_be_bytes(len_buf.try_into().expect("error to int")) as usize
 }
 
-fn extract_string(offset: usize, raw_params: &Vec<u8>) -> (String, usize) {
-    let offset = offset + 3; // 3 = 1(delimeter) + 2(type)
+fn extract_string(raw_params: &[u8]) -> (String, &[u8]) {
+    let offset = DATA_OFFSET;
     let len_buf = &raw_params[offset..offset + LENGTH_SIZE];
-    let length = usize::from_be_bytes(len_buf.try_into().expect("err to int"));
+    let length = u32::from_be_bytes(len_buf.try_into().expect("err to int")) as usize;
     let param_buf = &raw_params[LENGTH_SIZE + offset..length + LENGTH_SIZE + offset];
     (
         std::str::from_utf8(param_buf)
             .expect("err to get param")
             .to_owned(),
-        (length + LENGTH_SIZE + offset) + 1,
+        &raw_params[length + LENGTH_SIZE + offset..],
     )
 }
 
-fn extract_integer(offset: usize, raw_params: &Vec<u8>) -> (i64, usize) {
-    let offset = offset + 3;
-    let param_buf = &raw_params[LENGTH_SIZE + offset..2 * LENGTH_SIZE + offset];
+fn extract_integer(raw_params: &[u8]) -> (i64, &[u8]) {
+    let offset = DATA_OFFSET;
+    let param_buf = &raw_params[LENGTH_SIZE + offset..INTEGER_SIZE + LENGTH_SIZE + offset];
     (
         i64::from_be_bytes(param_buf.try_into().expect("err to ")),
-        (2 * LENGTH_SIZE + offset),
+        &raw_params[INTEGER_SIZE + LENGTH_SIZE + offset..],
     )
 }
 
-fn extract_string_list(offset: usize, raw_params: &Vec<u8>) -> (Vec<String>, usize) {
-    let offset = offset + 3;
-    let mut strings = Vec::with_capacity(raw_params[offset - 1] as usize);
+fn extract_string_list(raw_params: &[u8]) -> (Vec<String>, &[u8]) {
+    let offset = DATA_OFFSET;
+    let mut strings = Vec::new();
     let len_buf = &raw_params[offset..offset + LENGTH_SIZE];
-    let length = usize::from_be_bytes(len_buf.try_into().expect("err to int"));
-    let param_buf = &raw_params[LENGTH_SIZE + offset..length + LENGTH_SIZE + offset];
-    let mut sub_offset = 0;
-    while sub_offset < param_buf.len() {
-        let r = extract_string(sub_offset, &param_buf.to_vec());
+    let length = u32::from_be_bytes(len_buf.try_into().expect("err to int")) as usize;
+    let mut param_buf = &raw_params[LENGTH_SIZE + offset..length + LENGTH_SIZE + offset];
+    while param_buf.len() > 0 {
+        let r = extract_string(&param_buf);
         strings.push(r.0);
-        sub_offset += r.1;
+        param_buf = r.1;
     }
-    (strings, offset + sub_offset + LENGTH_SIZE)
+    (strings, &raw_params[length + LENGTH_SIZE + offset..])
 }
 
-fn extract_integer_list(offset: usize, raw_params: &Vec<u8>) -> (Vec<i64>, usize) {
-    let offset = offset + 3;
+fn extract_integer_list(raw_params: &[u8]) -> (Vec<i64>, &[u8]) {
+    let offset = DATA_OFFSET;
     let mut integers = Vec::new();
     let len_buf = &raw_params[offset..offset + LENGTH_SIZE];
-    let length = usize::from_be_bytes(len_buf.try_into().expect("err to int"));
-    let param_buf = &raw_params[LENGTH_SIZE + offset..length + LENGTH_SIZE + offset];
-    let mut sub_offset = 0;
-    while sub_offset < param_buf.len() {
-        let r = extract_integer(sub_offset, &param_buf.to_vec());
+    let length = u32::from_be_bytes(len_buf.try_into().expect("err to int")) as usize;
+    let mut param_buf = &raw_params[LENGTH_SIZE + offset..length + LENGTH_SIZE + offset];
+    while param_buf.len() > 0 {
+        let r = extract_integer(&param_buf);
         integers.push(r.0);
-        sub_offset = r.1;
+        param_buf = r.1;
     }
-    println!("len integer defail{:?}", integers);
-    println!("new offset {:?}", offset + sub_offset);
-    (integers, offset + sub_offset + LENGTH_SIZE)
+    (integers, &raw_params[length + LENGTH_SIZE + offset..])
 }
 
 pub fn extract_params(
@@ -71,11 +69,10 @@ pub fn extract_params(
     }
     let mut raw_params = vec![0; length];
     stream.read(&mut raw_params).expect("err get params");
-    println!("{:?}", raw_params);
-    let offset = 0;
-    let (pages, offset) = extract_integer_list(offset, &raw_params);
-    let (range, offset) = extract_integer_list(offset, &raw_params);
-    let (terms, offset) = extract_string_list(offset, &raw_params);
-    let (q, _) = extract_string_list(offset, &raw_params);
+    let raw_params = &raw_params[0..];
+    let (pages, raw_params) = extract_integer_list(raw_params);
+    let (range, raw_params) = extract_integer_list(raw_params);
+    let (terms, raw_params) = extract_string_list(raw_params);
+    let (q, _) = extract_string_list(raw_params);
     Ok((pages, range, terms, q))
 }
--- src/ipc/encode.rs
+++ src/ipc/encode.rs
@@ -1,10 +1,8 @@
 pub fn encode_result(result: String) -> Vec<u8> {
-    let mut bytes: Vec<u8> = vec![0; result.len() + 1 + 8];
+    let mut bytes: Vec<u8> = vec![0; result.len() + 2 + 4 * 2];
     let ret_len = result.len() as u32;
-    // let mut bytes: Vec<u8> = Vec::with_capacity(result.len() + 1 + 8);
-    bytes[9..].clone_from_slice(result.as_bytes());
-    bytes[5..9].clone_from_slice(&ret_len.to_be_bytes());
-    bytes[4] = 0;
-    bytes[0..4].clone_from_slice(&(ret_len + 5).to_be_bytes());
+    bytes[10..].clone_from_slice(result.as_bytes());
+    bytes[6..10].clone_from_slice(&ret_len.to_be_bytes());
+    bytes[0..4].clone_from_slice(&(ret_len + 6).to_be_bytes());
     bytes
 }
